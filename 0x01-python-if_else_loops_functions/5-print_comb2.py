e4b8a4703848463858634def168cf8a216470db7
soft-insight <j0108rodriguez@gmail.com>
2022-05-03 18:54:03 -0500

    correct requierements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if len(str(i)) == 1:
-        print(f'0{i}', end=', ')
-    elif i == len(range(0, 100)) - 1:
-        print(i)
-    else:
-        print(i, end=", ")
+for i in range(0, 99):
+    print(f'{i:02d}', end=', ')
+print(99)
+
