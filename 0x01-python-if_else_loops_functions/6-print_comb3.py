6b7794f767be3550d6a95d3c68c680dfa512388a
soft-insight <j0108rodriguez@gmail.com>
2022-05-03 18:41:04 -0500

    combnation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for number in range(0, 10):
     for number2 in range(0, 10):
         if number < number2:
             if not (number == 8 and number2 == 9):
-                print(f'{number}{number2} ', end='')
+                print(f'{number}{number2}', end=', ')
             else:
                 print(f'{number}{number2}', end='')
 print('')
