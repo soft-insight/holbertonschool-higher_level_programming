3837e3f5792da87fae46d0153be5a17412450f09
soft-insight <j0108rodriguez@gmail.com>
2022-05-03 18:39:02 -0500

    combnation

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for number in range(0, 10):
+    for number2 in range(0, 10):
+        if number < number2:
+            if not (number == 8 and number2 == 9):
+                print(f'{number}{number2} ', end='')
+            else:
+                print(f'{number}{number2}', end='')
+print('')
