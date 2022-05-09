221fb5f624a1776df126c50d18ecde299c95ca44
soft-insight <j0108rodriguez@gmail.com>
2022-05-09 16:32:04 -0500

    list tuples and more

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    [print(my_list[i]) for i in range(len(my_list))]
---
+++ 0x03-python-data_structures/4-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+new_in_list = __import__('4-new_in_list').new_in_list
+
+list = [1, 2, 3, 4, 5]
+
+A = new_in_list(list, 0, -7)
+print(list)
+print(A)
