ac19baea7d5236e8d7ff9c14a81f3b171139d5ea
soft-insight <j0108rodriguez@gmail.com>
2022-05-09 16:43:35 -0500

    coorect output

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [1, -4, 2, 3, 4, 5, 6]
+my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
