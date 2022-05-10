ae7182bb9f6a826c20b3ebd5df954db61bb6a56f
soft-insight <j0108rodriguez@gmail.com>
2022-05-09 20:26:36 -0500

    correct output

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 1:
-        tuple_a = (tuple_a[0], 0)
-        (a, b) = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-        (a, b) = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    if len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-        (a, b) = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-        (a, b) = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    # if len(tuple_a) == 0 and len(tuple_b) == 0:
-    #     (a, b) = (0, 0)
-
-    (a, b) = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
+    if len(tuple_a) < 2:
+        y = 0
+        if len(tuple_a) == 1:
+            y = tuple_a[0]
+        tuple_a = ((y), 0)
+    if len(tuple_b) < 2:
+        y = 0
+        if len(tuple_b) == 1:
+            y = tuple_b[0]
+        tuple_b = ((y), 0)
+    (a, b) = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return (a, b)
