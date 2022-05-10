5525cc7cf4f9ec1d1540aff0c0eb225793585340
soft-insight <j0108rodriguez@gmail.com>
2022-05-10 16:10:23 -0500

    print dict sorted by keys -- corrected output

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return dict(sorted(a_dictionary.items()))
+    A = dict(sorted(a_dictionary.items()))
+    for key, value in A.items():
+        print(f"{key}: {value}")
