5917c03e52b72644bac984fc12c6845cde6112f6
soft-insight <j0108rodriguez@gmail.com>
2022-05-10 18:55:12 -0500

    max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     A = list(a_dictionary.values())
     return list(a_dictionary.keys())[A.index(max(A))]
-
-
