36eee841823ddcfc6b4dfb7b6d51e67df413fe07
soft-insight <j0108rodriguez@gmail.com>
2022-05-10 12:57:56 -0500

    correct pystyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    A = [[matrix[j][i]**2 for i in range(len(matrix[0]))] for j in range(len(matrix))] 
+    a = len(matrix[0])
+    b = len(matrix)
+    A = [[matrix[j][i]**2 for i in range(a)] for j in range(b)]
     return A
