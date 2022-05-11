40d392ec48bddb8cc6bb7aa606436d63e4880e7a
soft-insight <j0108rodriguez@gmail.com>
2022-05-10 20:14:57 -0500

    Romans to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = [roman_string[i] for i in range(len(roman_string))] #separates the string
+    integersRoman = [symbol.get(j) for j in roman]
+    return sum(list(integersRoman))
+   
