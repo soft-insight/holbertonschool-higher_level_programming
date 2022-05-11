d9fdad8674901c4d12713d75711ad9c73d0b8ec9
soft-insight <j0108rodriguez@gmail.com>
2022-05-10 20:27:20 -0500

    Romans to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str):
-        symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        roman = [roman_string[i] for i in range(len(roman_string))]
-        integersRoman = [symbol.get(j) for j in roman]
-        return sum(integersRoman)
-    return 0
+    if isinstance(roman_string, str) is False or roman_string is None:
+        return 0
+    symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = [roman_string[i] for i in range(len(roman_string))]
+    integersRoman = [symbol.get(j) for j in roman]
+    return sum(integersRoman)
