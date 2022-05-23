d00a6a7a412f287aa98c5dad7150fb81b67e70fd
soft-insight <j0108rodriguez@gmail.com>
2022-05-23 16:07:08 -0500

    safe list of integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        my_list[x]
-        j = 0
-        for i in range(x + 1):
-            try:
-                print('este es j: ', j)
-                print(int(my_list[i]))
-                j += 1
-            except ValueError:
-                continue
-        return j
-    except IndexError:
-        j = 0
-        for i in my_list:
-            try:
-                print(int(i))
-                j += 1
-            except ValueError:
-                continue
-        return j
+    j = 0
+    for i in range(x):
+        try: 
+            print("{:d}".format(my_list[i]), end = '')
+            j += 1
+        except (TypeError,  ValueError):
+            continue
+    print()
+    return j
