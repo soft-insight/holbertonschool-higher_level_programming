b8dd64eb1f38ca09cbdf240e9c597fa2539b6e8b
soft-insight <j0108rodriguez@gmail.com>
2022-05-24 18:43:54 -0500

    class with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class with 'size' attribute"""
+
+
+class Square:
+    """Private instace attribute """
+    def __init__(self, size):
+        """
+        @size:  without type, represents
+                the area of the square
+        """
+        self.__size = size
