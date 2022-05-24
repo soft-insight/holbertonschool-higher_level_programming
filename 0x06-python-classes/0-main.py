8ec5d2f7c7ac83aeba9dd966c5af005cb740baf2
soft-insight <j0108rodriguez@gmail.com>
2022-05-24 18:26:38 -0500

    a class for square

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -4,4 +4,4 @@ Square = __import__('0-square').Square
 my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
-print(my_square.__doc__)
+print(Square.__doc__)
--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ An empty class"""
+
+
 class Square:
     """
     A class for an square
