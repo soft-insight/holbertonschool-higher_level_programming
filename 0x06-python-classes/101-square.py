1643b9597e3c8dcfda33a01343060516dd71b6f8
soft-insight <j0108rodriguez@gmail.com>
2022-05-25 11:06:47 -0500

    correct last enter

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -68,18 +68,15 @@ class Square:
 
     def __str__(self):
         """Print the square with the # character."""
-        if self.__size == 0:
-            print("")
-            return ("")
-
-        for k in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            for x in range(self.__position[0]):
-                print(" ", end="")
-            for j in range(self.__size):
-                print('#', end="")
-            if i != self.__size - 1:
+        if self.__size != 0:
+            for k in range(self.__position[1]):
                 print("")
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print('#', end="")
+                if i != self.__size - 1:
+                    print("")
 
         return ("")
