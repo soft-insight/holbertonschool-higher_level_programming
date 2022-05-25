be42ada47895d6ddd13600d4eec62a5a5f03e1ab
soft-insight <j0108rodriguez@gmail.com>
2022-05-24 22:24:21 -0500

    correct negative position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,7 +16,6 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
-        Args:
             size (int): The size of the new square.
         """
         self.size = size
@@ -41,19 +40,24 @@ class Square:
         return (self.__position)
 
     @position.setter
+# def position(self, value):
+#     if type(value) != tuple and len(value) != 2:
+#         raise TypeError('position must be a tuple of 2 positive integers')
+#     if type(value[0]) != int and type(value[1]) != int:
+#         raise TypeError('position must be a tuple of 2 positive integers')
+#     if value[0] < 0 or value[1] < 0:
+#         raise TypeError('position must be a tuple of 2 positive integers')
     def position(self, value):
-        if type(value) != tuple and len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if type(value[0]) != int and type(value[1]) != int:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError('position must be a tuple of 2 positive integers')
-
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
         """Return the current area of the square."""
-        return self.__size ** 2
+        return (self.__size ** 2)
 
     def my_print(self):
         """Print the square with the # character."""
