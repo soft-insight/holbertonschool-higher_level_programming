f7c5d32c5271fb42ed574ac263033c426009ec27
soft-insight <j0108rodriguez@gmail.com>
2022-05-24 22:18:55 -0500

    correct negative position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,11 +61,17 @@ class Square:
             print("")
             return
 
-        for k in range(self.__position[1]):
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
-        for i in range(self.__size):
-            for x in range(self.__position[0]):
-                print(" ", end="")
-            for j in range(self.__size):
-                print('#', end='')
-            print("")
+
+    # for k in range(self.__position[1]):
+        #     print("")
+        # for i in range(self.__size):
+        #     for x in range(self.__position[0]):
+        #         print(" ", end="")
+        #     for j in range(self.__size):
+        #         print('#', end='')
+        #     print("")
