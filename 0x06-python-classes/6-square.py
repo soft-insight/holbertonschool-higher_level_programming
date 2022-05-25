3787d6d479a070b252378c1d7d30d77e64f9dd7a
soft-insight <j0108rodriguez@gmail.com>
2022-05-24 22:30:27 -0500

    correct negative position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,12 +49,6 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
 
         self.__position = value
-# def position(self, value):
-#         if (not isinstance(value, tuple) or
-#                 len(value) != 2 or
-#                 not all(isinstance(num, int) for num in value) or
-#                 not all(num >= 0 for num in value)):
-#             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """Return the current area of the square."""
