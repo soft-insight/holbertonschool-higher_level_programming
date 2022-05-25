4cd8cd829f4502c3d61fca9d26439fa48296e8b4
soft-insight <j0108rodriguez@gmail.com>
2022-05-25 10:36:23 -0500

    commit: 0

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -54,17 +54,17 @@ class Square:
         """Return the current area of the square."""
         return (self.__size ** 2)
 
-    # def my_print(self):
-    #     """Print the square with the # character."""
-    #     if self.__size == 0:
-    #         print("")
-    #         return
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
 
-    #     [print("") for i in range(0, self.__position[1])]
-    #     for i in range(0, self.__size):
-    #         [print(" ", end="") for j in range(0, self.__position[0])]
-    #         [print("#", end="") for k in range(0, self.__size)]
-    #         print("")
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
     def __str__(self):
         """Print the square with the # character."""
