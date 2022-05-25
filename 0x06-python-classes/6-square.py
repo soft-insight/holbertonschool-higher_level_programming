80ed69a65d1b256fd77fb6d1de2b017c71f4082f
soft-insight <j0108rodriguez@gmail.com>
2022-05-24 22:29:36 -0500

    correct negative position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,7 +43,7 @@ class Square:
     def position(self, value):
         if type(value) != tuple or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if type(value[0]) != int and type(value[1]) != int:
+        if type(value[0]) != int or type(value[1]) != int:
             raise TypeError('position must be a tuple of 2 positive integers')
         if value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
