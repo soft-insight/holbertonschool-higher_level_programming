00c11037620dbd981aa5391b1520af60399df14e
soft-insight <j0108rodriguez@gmail.com>
2022-05-31 12:17:55 -0500

    commit: 0

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Matrix multiplication 
+""" Matrix multiplication
     with numpy module
 """
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """ Multiplication of 
+    """ Multiplication of
         two conformables matrices
     """
-    
+
     import numpy as np
-    
+
     return np.matmul(m_a, m_b)
