4782876cce3623cecfcd7e713bf9d3b4a470d6ee
soft-insight <j0108rodriguez@gmail.com>
2022-05-31 10:19:32 -0500

    commit: 0

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    indent after ., ?, or :
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if text[i] is ' ' and text[i - 1] in {'.', ':', '?'}:
+            continue
+        
+        print(text[i], end='')
+        if text[i] == '.' or text[i] == ':' or text[i] == '?':
+            print('\n')
