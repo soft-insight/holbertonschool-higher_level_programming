c925a787af7771df236c91025cd126ed3bee9556
soft-insight <j0108rodriguez@gmail.com>
2022-05-31 11:50:38 -0500

    commit: 0

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unittest for max_integer
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    A class to test a max integer function
+    """
+
+    def test_max_integer(self):
+        """ Test correct results
+        """
+        self.assertIsNone(max_integer([]))
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertAlmostEqual(max_integer([-4, -3, -2, -1, 0]), 0)
+        self.assertAlmostEqual(max_integer([-90, -120, -150, -180]), -90)
+        self.assertAlmostEqual(max_integer([1.0, 1.5, 1.6, 3.7, 2.3]), 3.7)
+        self.assertAlmostEqual(max_integer([7.7]), 7.7)
+
+    def test_wrong_types(self):
+        """ Wrong  types
+        """
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+        with self.assertRaises(TypeError):
+            max_integer(["Holb", 5, 3, 0, "Python"])
