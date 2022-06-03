29ed1259805bf7e339311da5fd0f39f9773c6356
soft-insight <j0108rodriguez@gmail.com>
2022-06-02 20:26:23 -0500

    cuál es el error?

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-'''A class for rectangle'''
+'''
+Rectangle class
+'''
 
 
 class Rectangle:
-    '''Rectangle with attributes
-        widht and height
-        Methods: area, perimeter and print rectangles with '#'
     '''
-
+    class that will contain a description of rectangle
+    '''
+    side = 0
     number_of_instances = 0
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
@@ -22,11 +23,12 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -34,33 +36,31 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
-        return self.__height * self.__width
+        return(self.__width * self.__height)
 
     def perimeter(self):
+        side = self.__width + self.__height
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * self.__height + 2 * self.__width
+        return(side + side)
 
     def __str__(self):
+        a = str(self.print_symbol)
         if self.__width == 0 or self.__height == 0:
-            return ''
-        string = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                string += str(self.print_symbol)
-            if i != self.__height - 1:
-                string += '\n'
-        return string
+            return ('')
+        return((a * self.__width + '{0}'.
+                format('\n')) * (self.__height - 1) + a * self.__width)
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         print("Bye rectangle...")
@@ -79,4 +79,4 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return (cls(size, size))
