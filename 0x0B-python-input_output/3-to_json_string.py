f4bc724fa66585a51ec8098264d245779c3f4c66
soft-insight <j0108rodriguez@gmail.com>
2022-06-08 20:14:31 -0500

    Python: input/output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!usr/bin/python3
 """ from json to string
 """
+import json
 
 
 def to_json_string(my_obj):
     """ input an JSON object
         returns a string
     """
-    import json
     return json.dumps(my_obj)
