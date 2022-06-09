160c56039f3084af56b53fae7e057ca7f8ef4784
soft-insight <j0108rodriguez@gmail.com>
2022-06-08 20:47:27 -0500

    Python: input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,13 @@
 """ Adds all arguments to a Python list
 """
 
+
 from sys import argv
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 try:
     list2add = load_from_json_file('add_item.json')
