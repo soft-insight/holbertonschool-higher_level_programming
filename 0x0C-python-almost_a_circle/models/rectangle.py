306a397e1183f37d842ccf4d2b157bd14bdf2db6
soft-insight <j0108rodriguez@gmail.com>
2022-06-14 20:59:43 -0500

    commit: 0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,51 +24,60 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ getter for width attribute """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter for width attribute"""
         self.validate_attribute(value, 'width')
         self.__width = value
 
     @property
     def height(self):
+        """getter for height attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter for height attribute"""
         self.validate_attribute(value, 'height')
         self.__height = value
 
     @property
     def x(self):
+        """getter for x attribute"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter for x attribute"""
         self.validate_attribute(value, 'x')
         self.__x = value
 
     @property
     def y(self):
+        """getter for y attribute"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter for y attribute"""
         self.validate_attribute(value, 'y')
         self.__y = value
 
     def area(self):
+        """method: area for rectangle class"""
         return self.width * self.height
 
     def __str__(self):
+        """format to print"""
         s1 = f"[Rectangle] ({self.id}) {self.x}/{self.y} "
         s2 = f"- {self.width}/{self.height}"
         return s1 + s2
 
     def display(self):
-        """ print strings for the rectangle
-        """
+        """ print strings for the rectangle"""
         for i in range(self.y):
             print()
         for i in range(self.height):
@@ -79,8 +88,7 @@ class Rectangle(Base):
             print()
 
     def update(self, *args, **kwargs):
-        """ *args and **kwargs
-        """
+        """ *args and **kwargs """
         lst = ['id', 'width', 'height', 'x', 'y']
         if args is not None and len(args) != 0:
             for i in range(len(args)):
@@ -90,8 +98,7 @@ class Rectangle(Base):
                 setattr(self, k, v)
 
     def to_dictionary(self):
-        """ returns a dict of the attributes.
-        """
+        """ returns a dict of the attributes """
         return {
             'id': self.id,
             'width': self.width,
@@ -102,8 +109,7 @@ class Rectangle(Base):
 
     @staticmethod
     def validate_attribute(value, attribute):
-        """ function in static method to validate attributes
-        """
+        """ function in static method to validate attributes """
         if type(value) is not int:
             raise TypeError(attribute + ' must be an integer')
 
