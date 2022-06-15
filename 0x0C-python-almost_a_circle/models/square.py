dc3b650f5681ee617481eaefa4d1ed8fa0fc3f03
soft-insight <j0108rodriguez@gmail.com>
2022-06-14 21:06:36 -0500

    commit: 0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,31 +5,36 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ class Square
+    ... heritages from Rectangle
+    ... same attributes with:
+                size = with or height
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """ constructor for Square """
+
         super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
-        """Print a string format
-        """
+        """ Print a string format """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        '''getter size
-        '''
+        """ getter for size """
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter size
-        """
+        """ setter for size attribute """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Args and kwargs
-        """
+        """ *args and **kwargs """
+
         lst = ['id', 'size', 'x', 'y']
         if args is not None and len(args) != 0:
             for i in range(len(args)):
