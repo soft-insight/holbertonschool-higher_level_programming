d996366fa0fe939be966b54f9390c606b9a0c81b
soft-insight <j0108rodriguez@gmail.com>
2022-07-13 13:59:25 -0500

    commit 0: order by

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,10 @@
+-- script that lists all records of the 
+-- table second_table of the database hbtn_0c_0 in your MySQL server.
+-- ORDER BY
+SELECT 
+	score, 
+	name 
+FROM 
+	second_table 
+ORDER BY 
+	score desc;
