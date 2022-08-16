2bb559ca93ec7504205a1fa16e2346035e3c5a44
soft-insight <j0108rodriguez@gmail.com>
2022-08-15 21:05:50 -0500

    commit 0

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar1 = 'C is fun';
+console.log(myVar1);
+const myVar2 = 'Python is cool';
+console.log(myVar2);
+const myVar = 'Javascript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+myObject.value = 89;
+console.log(myObject);
