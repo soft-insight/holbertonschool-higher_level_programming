5364fb16cc47ea9ed8c09fe077b3e381c610c5f5
soft-insight <j0108rodriguez@gmail.com>
2022-08-15 22:37:15 -0500

    commit 0

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,5 @@ const myVar1 = 'C is fun';
 console.log(myVar1);
 const myVar2 = 'Python is cool';
 console.log(myVar2);
-const myVar = 'Javascript is amazing';
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
