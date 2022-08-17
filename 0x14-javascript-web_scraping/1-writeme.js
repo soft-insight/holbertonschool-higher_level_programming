2b8060f734c01775974cce0019a8e9fe420233dd
soft-insight <j0108rodriguez@gmail.com>
2022-08-17 18:14:30 -0500

    commit: 0

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(file, text, 'utf8', function(err, data) {
+fs.writeFile(file, text, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   }
 });
-
-
