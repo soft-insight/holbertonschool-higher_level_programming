e82081bf730ff0f48420c1feabeeeb62e4862c26
soft-insight <j0108rodriguez@gmail.com>
2022-08-18 06:54:54 -0500

    commit: 0

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ const url = process.argv[2];
 
 axios.get(url)
   .then(function (response) {
-    let lgth = response.data.results.length;
+    const lgth = response.data.results.length;
     let movie = 0;
     for (let i = 0; i < lgth; i++) {
       if (response.data.results[i].characters.includes(antilles)) {
